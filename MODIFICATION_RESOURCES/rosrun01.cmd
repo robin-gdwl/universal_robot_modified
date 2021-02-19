a63b2fb3ecffd63f118016a7dcd0cd3eccb18dd1
Robin Godwyll <rodwyll+github@gmail.com>
2021-02-19 13:33:42 +0100

    changed to the RWTH Gitlab repository
    https://git-ce.rwth-aachen.de/henrik.hose/universal_robot/-/tree/calibration_devel/ur_e_description

--- MODIFICATION_RESOURCES/rosrun01.cmd
+++ MODIFICATION_RESOURCES/rosrun01.cmd
@@ -7,10 +7,11 @@ docker run -v D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\u
 
 docker run -v D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot:/xacro ros_ur:latest rosrun xacro xacro --inorder /xacro/ur_e_description/urdf/ur3e_robot.urdf.xacro > D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot\ur_e_description\urdf\ur3e_robot.urdf
 
+docker run -v D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot:/xacro ros_ur:latest rosrun xacro xacro --inorder /xacro/ur_e_description/urdf/ur16e_robot.urdf.xacro > D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot\ur_e_description\urdf\ur16e_robot.urdf
+
+
 @REM rosrun xacro xacro --inorder D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot\ur_e_description\urdf\ur10e_robot.urdf.xacro > model.urdf
 @REM docker run -it --rm -v D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot:/work osrf/ros:kinetic-desktop
-
-
 
 @REM docker run -v D:\Robins_Data\Projects\Assorted_CADscripts\load_urdf_into_rhino\universal_robot\ur_e_description:/xacro osrf/ros:kinetic-desktop-full rosrun xacro xacro --inorder /xacro/urdf/ur10e_robot.urdf.xacro > panda_arm_hand.urdf
 
